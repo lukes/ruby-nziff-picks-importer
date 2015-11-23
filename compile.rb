871cbb8bc34192deeae604cc74e0541967ea5acd
Luke Duncalfe <lduncalfe@eml.cc>
2015-11-23 14:52:53 +1300

    Compiling to a single data.json file.

--- compile.rb
+++ compile.rb
@@ -1,33 +1,22 @@
 # compiles saved data into ember fixtures
+require 'active_support/json'
+require 'active_support/core_ext/hash'
 
-PATH = File.join(File.dirname(__FILE__), '../app/scripts/data')
+PATH = File.dirname(__FILE__)
 
 # retrieve data from files
-films = Dir.glob('imported/films/rotten_tomatoes/*.json').map do |f|
-  File.read(f)
+films = Dir.glob(File.join(PATH, 'imported/films/rotten_tomatoes', '*.json')).map do |f|
+  ActiveSupport::JSON.decode(File.read(f)).with_indifferent_access
 end
 
-reviews = Dir.glob('imported/reviews/rotten_tomatoes/*.json').map do |f|
-  File.read(f)
+reviews = Dir.glob(File.join(PATH, 'imported/reviews/rotten_tomatoes', '*.json')).map do |f|
+  ActiveSupport::JSON.decode(File.read(f)).with_indifferent_access
 end
 
-# write data as fixtures
-File.open(File.join(PATH, 'films.js'), 'w') do |f|
-  fixtures = "NziffPicks.Film.FIXTURES = [#{films.join(',')}];"
-  f.write(fixtures)
-end
-
-File.open(File.join(PATH, 'reviews.js'), 'w') do |f|
-  fixtures = ['NziffPicks.Review.FIXTURES = [',reviews.join(','),'];'].join
-  f.write(fixtures)
-end
-
-# temporarily make all films belong to auckland
-File.open(File.join(PATH, 'regions.js'), 'w') do |f|
-  film_ids = Dir.glob('imported/films/rotten_tomatoes/*.json').map do |file|
-    file.sub(/\D+(\d+)\D+/, '\1')
-  end
-  regions = ["{ id: 'auckland', name: 'Auckland', films: [#{film_ids.join(',')}] }","{ id: 'wellington', name: 'Wellington', films: [] }"]
-  fixtures = ['NziffPicks.Region.FIXTURES = [',regions.join(','),'];'].join
-  f.write(fixtures)
+# write data
+File.open(File.join(PATH, 'compiled', 'data.js'), 'w+') do |f|
+  data = {}
+  data[:films] = films
+  data[:reviews] = reviews
+  f.write(ActiveSupport::JSON.encode(data))
 end
